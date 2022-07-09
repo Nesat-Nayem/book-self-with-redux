523b87e4829892c66cb0c85ad467fbabe36eb688
unknown <nesatnayem042@gmail.com>
2022-07-09 10:13:02 +0600

    get data and set data useselector

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import { 
   HiPlusCircle, 
   // HiMinusCircle,
   // HiCheckCircle 
 } from 'react-icons/hi';
 import styles from './book.module.css'
+import { addtoReadingList } from "../../redux/actions/bookAction";
 const SingleBook = (props) => {
+
   const { title, author, coverImageUrl, synopsis } = props.book;
+  const dispatch = useDispatch()
   return (
     <div className='card d-flex mb-3 p-3' 
       style={{position: 'relative'}}
@@ -25,7 +29,7 @@ const SingleBook = (props) => {
       </div>
       <div className={styles.control_icons} >
         {/* <HiMinusCircle title="Remove from list" className={styles.minus_icon} /> */}
-        <HiPlusCircle title="Add to Reading" className={styles.plus_icon} />
+        <HiPlusCircle onClick={()=>dispatch(addtoReadingList(props.book))} title="Add to Reading" className={styles.plus_icon} />
         {/* <HiCheckCircle title="Mark as Finish" className={styles.check_icon} /> */}
       </div>
     </div>
